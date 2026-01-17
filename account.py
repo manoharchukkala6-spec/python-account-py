08ee6f4b2fc5057f40859e0c6bf41923c0577629
manohar chukkala <manoharchukkala6@gmail.com>
2026-01-17 12:03:24 +0530

    Add files via upload

---
+++ account.py
@@ -0,0 +1,17 @@
+print("enter your personal account:")
+accountno = 2360614
+enter =int(input("enter your account number"))
+
+name = "manohar"
+enterna =str(input("enter your name"))
+
+if enter == accountno:
+    print("thank you enter for right account number:")
+elif name == enterna:
+    print("thank you for enter your name as right:")
+else:
+    print("its unfind:")
+
+    
+
+
